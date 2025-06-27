59156c9809b150e39f9efe2b9170ef8d3ba42812
Ali Siddiqi <alisiddiqui275@gmail.com>
2025-06-27 15:54:58 +0500

    Pagination

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ Route::get('/', function () {
 
 Route::get('/jobs', function () {
 
-    $jobs = Job::with('employer')->get();
+    $jobs = Job::with('employer')->simplePaginate(2);
     return view('jobs', [
 
         'jobs'=> $jobs
